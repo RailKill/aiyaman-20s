f616f878d6ebf171f2218582694ed117017f2e0f
Lemoney <railkill@gmail.com>
2022-11-27 18:01:05 +0800

    Add rotation

--- app/main.rb
+++ app/main.rb
@@ -1,11 +1,21 @@
 def tick args
   # Initialize state of interactable sprites in the game. The "||=" operator sets the value only if uninitialized.
   # Which means, it will only run once at the beginning of the game.
+  args.state.angles ||= {}
+  args.state.angles["gas"] ||= [0, -90]
+  args.state.angles["lightbowl"] ||= [0, -45, 45]
+  args.state.angles["magnet"] ||= [0, -90, -180, -270]
+  args.state.angles["table"] ||= [0, -45, 45]
+
+  args.state.birdflip ||= 0
   args.state.highlights ||= {}
+  args.state.rotations ||= {}
   args.state.timer ||= args.state.tick_count
 
+  # Draw aiyaman.
   one_time_animation args
 
+  # Draw timer.
   args.outputs.labels << {x: 170, y: 312, text: "%05.2f" % countdown(args).to_s, size_enum: 8,
       r: args.state.highlights["clock"] == 1 ? 225 : 0}
 
@@ -18,8 +28,8 @@ def tick args
   draw_interactable(args, "birdy", {x: 440, y: 616, w: 56, h: 82}, "birdstand")
   draw_interactable(args, "clock", {x: 148, y: 262, w: 140, h: 66})
   draw_interactable(args, "frame", {x: 890, y: 440, w: 184, h: 220})
-  draw_interactable(args, "gas", {x: 1050, y: 60, w: 128, h: 260})
-  draw_interactable(args, "lightbowl", {x: 635, y: 550, w: 70, h: 48})
+  draw_interactable(args, "gas", {x: 1050, y: 60, w: 128, h: 260, angle_anchor_y: 0.2})
+  draw_interactable(args, "lightbowl", {x: 635, y: 550, w: 70, h: 48, angle_anchor_y: 0.7})
   draw_interactable(args, "lightfixture", {x: 660, y: 580, w: 21, h: 140})
   draw_interactable(args, "magnet", {x: 230, y: 470, w: 78, h: 88})
   draw_interactable(args, "pizzamonster", {x: 903, y: 484, w: 154, h: 130}, "frame")
@@ -84,6 +94,33 @@ def draw_interactable(args, id, rect, combine = id)
   # Highlight sprite when mouse is inside the sprite's rectangle.
   args.state.highlights[id] = (args.inputs.mouse.inside_rect? rect) ? 1 : 0
 
+  # Update the rotation of the sprite.
+  rotated = rotate(args, id, rect)
+
   # Draw the sprite.
-  args.outputs.sprites << rect.merge({path: "sprites/#{id}-#{args.state.highlights[combine]}.png"})
+  args.outputs.sprites << rotated.merge({path: "sprites/#{id}-#{args.state.highlights[combine]}.png"})
+end
+
+
+# Rotate a given rect of an interactable if interact button is triggered.
+def rotate(args, id, rect)
+  # Some interactables don't have initial values declared, we'll rectify that here.
+  args.state.angles[id] ||= []
+  args.state.rotations[id] ||= 0
+
+  # Get the array which specifies the rotation angles available for the given id.
+  limit = args.state.angles[id].length - 1
+
+  # Increment the rotation index by one if there is a mouse click on the interactable.
+  is_mouse_over = args.inputs.mouse.inside_rect? rect
+  increment = args.state.rotations[id] + (is_mouse_over && args.inputs.mouse.click ? 1 : 0)
+
+  # Try to increment the index now, but reset to 0 if exceeds the length of the limit array.
+  next_index = args.state.angles[id] && increment <= limit ? increment : 0
+  
+  # Update the rotation index state in args.
+  args.state.rotations[id] = next_index
+
+  # Merge the angle into the rect hash so that it can be drawn.
+  return rect.merge({angle: args.state.angles[id][next_index]})
 end
