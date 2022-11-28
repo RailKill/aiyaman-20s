3da00568ac89ee5944467f8cd312fc00509f3ff3
Lemoney <railkill@gmail.com>
2022-11-28 23:42:04 +0800

    Add sounds and music

--- app/main.rb
+++ app/main.rb
@@ -2,31 +2,31 @@ def tick args
   # Initialize state of interactable sprites in the game. The "||=" operator sets the value only if uninitialized.
   # Which means, it will only run once at the beginning of the game.
   args.state.angles ||= {}
-  args.state.angles["gas"] ||= [0, -90]
-  args.state.angles["lightbowl"] ||= [0, -45, 45]
-  args.state.angles["magnet"] ||= [0, -90, -180, -270]
-  args.state.angles["table"] ||= [0, -45, 45]
+  args.state.angles['gas'] ||= [0, -90]
+  args.state.angles['lightbowl'] ||= [0, -45, 45]
+  args.state.angles['magnet'] ||= [0, -90, -180, -270]
+  args.state.angles['table'] ||= [0, -45, 45]
   args.state.birdflip ||= 0
   args.state.highlights ||= {}
   args.state.rotations ||= {}
   args.state.timer ||= args.state.tick_count
 
   # Draw static sprites.
-  args.outputs.sprites << {x: 0, y: 120, w: 1280, h: 660, path: "sprites/wall.png"}
-  args.outputs.sprites << {x: 0, y: 60, w: 300, h: 342, path: "sprites/lab.png"}
+  args.outputs.sprites << {x: 0, y: 120, w: 1280, h: 660, path: 'sprites/wall.png'}
+  args.outputs.sprites << {x: 0, y: 60, w: 300, h: 342, path: 'sprites/lab.png'}
   
   # Draw interactables.
-  draw_interactable(args, "birdstand", {x: 420, y: 592, w: 90, h: 128})
-  draw_interactable(args, "birdy", {x: 440, y: 616, w: 56, h: 82}, "birdstand")
-  draw_interactable(args, "clock", {x: 148, y: 262, w: 140, h: 66})
-  draw_interactable(args, "frame", {x: 890, y: 440, w: 184, h: 220})
-  draw_interactable(args, "gas", {x: 1050, y: 60, w: 128, h: 260, angle_anchor_y: 0.2})
-  draw_interactable(args, "lightbowl", {x: 635, y: 550, w: 70, h: 48, angle_anchor_y: 0.7})
-  draw_interactable(args, "lightfixture", {x: 660, y: 580, w: 21, h: 140})
-  draw_interactable(args, "magnet", {x: 230, y: 470, w: 78, h: 88})
-  draw_interactable(args, "pizzamonster", {x: 903, y: 484, w: 154, h: 130}, "frame")
-  draw_interactable(args, "table", {x: 540, y: 60, w: 248, h: 165})
-  draw_interactable(args, "tnp", {x: 905, y: 481, w: 140, h: 164}, "frame")
+  draw_interactable(args, 'birdstand', {x: 420, y: 592, w: 90, h: 128})
+  draw_interactable(args, 'birdy', {x: 440, y: 616, w: 56, h: 82}, 'birdstand')
+  draw_interactable(args, 'clock', {x: 148, y: 262, w: 140, h: 66})
+  draw_interactable(args, 'frame', {x: 890, y: 440, w: 184, h: 220})
+  draw_interactable(args, 'gas', {x: 1050, y: 60, w: 128, h: 260, angle_anchor_y: 0.2})
+  draw_interactable(args, 'lightbowl', {x: 635, y: 550, w: 70, h: 48, angle_anchor_y: 0.7})
+  draw_interactable(args, 'lightfixture', {x: 660, y: 580, w: 21, h: 140})
+  draw_interactable(args, 'magnet', {x: 230, y: 470, w: 78, h: 88})
+  draw_interactable(args, 'pizzamonster', {x: 903, y: 484, w: 154, h: 130}, 'frame')
+  draw_interactable(args, 'table', {x: 540, y: 60, w: 248, h: 165})
+  draw_interactable(args, 'tnp', {x: 905, y: 481, w: 140, h: 164}, 'frame')
 
   # Draw aiyaman.
   draw_aiyaman args
@@ -36,7 +36,7 @@ def tick args
 
   # Draw timer.
   args.outputs.labels << {x: 170, y: 312, text: "%05.2f" % countdown(args).to_s, size_enum: 8,
-      r: args.state.highlights["clock"] == 1 ? 225 : 0}
+      r: args.state.highlights['clock'] == 1 ? 225 : 0}
 
   # Prompts player to start the game.
   start_game args
@@ -96,12 +96,12 @@ def draw_jimmy args
     current_y = 40
 
     # Draw the sprite at the interpolated position.
-    args.outputs.sprites << { x: current_x, y: current_y, w: 312, h: 386, path: "sprites/jimmy/throw-0.png"}
+    args.outputs.sprites << { x: current_x, y: current_y, w: 312, h: 386, path: 'sprites/jimmy/throw-0.png'}
 
     # Draw jimmy's equipments together with him as well.
-    args.outputs.sprites << { x: current_x - 40, y: current_y + 230, w: 76, h: 46, path: "sprites/gun-0.png" }
-    args.outputs.sprites << { x: current_x + 270, y: current_y + 310, w: 56, h: 66, path: "sprites/shuriken.png" }
-    args.outputs.sprites << { x: current_x + 130, y: current_y + 310, w: 60, h: 20, path: "sprites/sunglasses.png" }
+    args.outputs.sprites << { x: current_x - 40, y: current_y + 230, w: 76, h: 46, path: 'sprites/gun-0.png' }
+    args.outputs.sprites << { x: current_x + 270, y: current_y + 310, w: 56, h: 66, path: 'sprites/shuriken.png' }
+    args.outputs.sprites << { x: current_x + 130, y: current_y + 310, w: 60, h: 20, path: 'sprites/sunglasses.png' }
   end
 end
 
@@ -116,8 +116,23 @@ def rotate(args, id, rect)
   limit = args.state.angles[id].length - 1
 
   # Increment the rotation index by one if there is a mouse click on the interactable.
-  is_mouse_over = args.inputs.mouse.inside_rect? rect
-  increment = args.state.rotations[id] + (args.state.is_playing && is_mouse_over && args.inputs.mouse.click ? 1 : 0)
+  is_rotated = args.state.is_playing && args.inputs.mouse.inside_rect?(rect) && args.inputs.mouse.click
+  increment = args.state.rotations[id] + (is_rotated ? 1 : 0)
+  
+  # Play sound if interactable is being rotated.
+  if is_rotated
+    # Some sounds have the same filename as the id, use those sounds for those interactables.
+    random_number = rand(3)
+    random_name = id == 'clock' ? 'sounds/clock.wav' : "sounds/#{id}-#{random_number}.wav"
+    
+    # Otherwise, use the normal metal turn sound.
+    sound_name = File.exists?("mygame/#{random_name}") ? random_name : "sounds/turn-#{random_number}.wav"
+
+    # For some overlapping interactables, they will be excluded from playing any sounds.
+    if !['tnp', 'pizzamonster', 'birdy'].include? id
+      args.outputs.sounds << sound_name
+    end
+  end
 
   # Try to increment the index now, but reset to 0 if exceeds the length of the limit array.
   next_index = args.state.angles[id] && increment <= limit ? increment : 0
@@ -134,12 +149,19 @@ end
 def start_game args
   if !args.state.is_playing
     # If the game has not started yet, display instructions.
-    args.outputs.labels <<  { x: 640, y: 360, text: "Press 'Space' to Start",
+    args.outputs.labels <<  { x: 640, y: 360, text: 'Press "Space" to Start',
         alignment_enum: 1, vertical_alignment_enum: 1 }
+
+    # Start a music loop at the beginning of the game. By default, .ogg files are looped forever.
+    args.audio[:music] ||= { input: 'sounds/reverse-time.ogg', looping: true, gain: 0.2 }
     
     if args.inputs.keyboard.key_down.space
       # If the player press space, set the game started flag to true.
       args.state.is_playing = true
+
+      # Stop currently playing music and load the time-stop music.
+      args.audio.delete :music
+      args.audio[:music] = { input: 'sounds/time-stop.ogg',  gain: 0.2 }
 
       # Reset animation and timers.
       args.state.animation_start = args.state.timer = args.state.tick_count
